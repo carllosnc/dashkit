b7aa73505e819c6b07612c9bbfb4ff7bdbba96a2
Carlos Costa <carllos.nc@gmail.com>
2026-03-16 23:44:03 -0300

    feat: setup react router in root provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { ButtonDocs } from './pages/ButtonDocs.tsx'
+import { DocsLayout } from './layouts/DocsLayout.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/docs/button" element={<ButtonDocs />} />
+        <Route path="/docs" element={<DocsLayout />}>
+          <Route path="button" element={<ButtonDocs />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
