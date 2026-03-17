0da7bf6e81dd10abf4a580b24ce72f3876c30b11
Carlos Costa <carllos.nc@gmail.com>
2026-03-16 23:43:54 -0300

    feat: design Button documentation page structure

--- src/pages/ButtonDocs.tsx
+++ src/pages/ButtonDocs.tsx
@@ -1,14 +1,9 @@
-import { Button } from '../components/Button';
+import { Button } from '../components/Button/Button';
 import { FiDownload, FiArrowRight } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
 
 export function ButtonDocs() {
   return (
-    <div className="max-w-4xl mx-auto p-12 text-left">
-      <Link to="/" className="text-indigo-600 dark:text-indigo-400 hover:underline mb-8 inline-block">
-        &larr; Back to Home
-      </Link>
-
+    <div className="max-w-3xl text-left">
       <h1 className="text-4xl font-bold mb-4">Button Component</h1>
       <p className="text-lg text-neutral-600 dark:text-neutral-400 mb-12">
         A highly customizable button component supporting icons and multiple variants.
