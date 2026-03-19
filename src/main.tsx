49999f4eebf6b89cb08638dd92a2cbc47359f19f
Carlos Costa <carllos.nc@gmail.com>
2026-03-18 22:10:41 -0300

    routing: add new component routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,10 @@ import ButtonDocs from './pages/button.mdx'
 import CheckboxDocs from './pages/checkbox.mdx'
 import InputDocs from './pages/input.mdx'
 import RadioDocs from './pages/radio.mdx'
+import SelectDocs from './pages/select.mdx'
+import SwitchDocs from './pages/switch.mdx'
+import TabsDocs from './pages/tabs.mdx'
+import OtpInputDocs from './pages/otp-input.mdx'
 import IntroductionDocs from './pages/introduction.mdx'
 import { DocsLayout } from './layouts/DocsLayout.tsx'
 import { MdxWrapper } from './layouts/MdxWrapper.tsx'
@@ -26,6 +30,10 @@ createRoot(document.getElementById('root')!).render(
             <Route path="input" element={<MdxWrapper Component={InputDocs} />} />
             <Route path="checkbox" element={<MdxWrapper Component={CheckboxDocs} />} />
             <Route path="radio" element={<MdxWrapper Component={RadioDocs} />} />
+            <Route path="select" element={<MdxWrapper Component={SelectDocs} />} />
+            <Route path="switch" element={<MdxWrapper Component={SwitchDocs} />} />
+            <Route path="tabs" element={<MdxWrapper Component={TabsDocs} />} />
+            <Route path="otp-input" element={<MdxWrapper Component={OtpInputDocs} />} />
           </Route>
         </Routes>
       </BrowserRouter>
